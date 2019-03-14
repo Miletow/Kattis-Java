9c2e84c43e76274341c4db25fb5f332461286589
Milton Blaesild <miletow@gmail.com>
2019-03-14 19:19:58 +0100

    commit

--- ABC.java
+++ ABC.java
@@ -1,6 +1,7 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.Arrays;
 
-public class aaah {
+public class ABC {
 
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
@@ -8,23 +9,36 @@ public class aaah {
 		int a = sc.nextInt();
 		int b = sc.nextInt();
 		int c = sc.nextInt();
-		
-		string a = sc.nextLine();
-		string b = sc.next();
-		string c = sc.next();
-   // solve test case and output answer
-   System.out.println(aaah(a, b));
-		}
+
+		int Numbers[] = {a, b, c};
+
+		char aS = sc.next().charAt(0);
+		char bS = sc.next().charAt(0);
+		char cS = sc.next().charAt(0);
+
+		char Letters[] = {aS, bS, cS};
+
+		ABC(Numbers, Letters);
+			}
     }
 	
-	public static String aaah(String jon, String doctor){
+	public static void ABC(int Numbers[],char Letters[]){
 		
-		if(jon.length() >= doctor.length()){
-		
-			return "go";
+		Arrays.sort(Numbers);
+
+		int A = Numbers[0];
+		int B = Numbers[1];
+		int C = Numbers[2];
+
+		for (char var : Letters) 
+		{ 
+			if(var == 'A')
+			System.out.print(A + " ");
+			else if(var == 'B')
+			System.out.print(B + " ");
+			else if(var == 'C')
+			System.out.print(C + " ");
 		}
-		
-		return "no";
 	}
 
 }
